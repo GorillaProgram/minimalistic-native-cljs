10a873a1aa8daa1559e0cfd7708698474711339a
MeePwn <maybewaityou@gmail.com>
2017-01-13 10:56:03 +0800

    first commit

--- src/minimalistic_native_cljs/app/core.cljs
+++ src/minimalistic_native_cljs/app/core.cljs
@@ -1,18 +1,13 @@
 (ns minimalistic-native-cljs.app.core
   (:require [reagent.core :as r :refer [atom]]
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+            [minimalistic-native-cljs.app.main.components.view.ui-components :refer [text view image touchable-highlight]]
             [minimalistic-native-cljs.app.main.modules.toast :refer [show-long-toast show-short-toast]]
             [minimalistic-native-cljs.app.main.modules.callback :refer [test-callback]]
             [minimalistic-native-cljs.app.dataflow.event.events]
             [minimalistic-native-cljs.app.dataflow.sub.subs]))
 
 (def ReactNative (js/require "react-native"))
-
-(def app-registry (.-AppRegistry ReactNative))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
 (def logo-img (js/require "./images/cljs.png"))
 
@@ -39,4 +34,4 @@
                :style  {:width 80 :height 80 :margin-bottom 30}}]
        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
                              :on-press #(handle-click "success")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Press Me"]]])))
+        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Press Me!!!"]]])))
---
+++ src/minimalistic_native_cljs/app/main/components/view/ui-components.cljs
@@ -0,0 +1,10 @@
+(ns minimalistic-native-cljs.app.main.components.view.ui-components
+  (:require [reagent.core :as r :refer [atom]]))
+
+(def ReactNative (js/require "react-native"))
+
+(def app-registry (.-AppRegistry ReactNative))
+(def text (r/adapt-react-class (.-Text ReactNative)))
+(def view (r/adapt-react-class (.-View ReactNative)))
+(def image (r/adapt-react-class (.-Image ReactNative)))
+(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
