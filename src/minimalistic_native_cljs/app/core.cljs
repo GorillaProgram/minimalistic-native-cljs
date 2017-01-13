5bb65ca19d9ccffa1744f97ce7b31650d573f449
MeePwn <maybewaityou@gmail.com>
2017-01-13 11:19:07 +0800

    first commit

--- src/minimalistic_native_cljs/app/core.cljs
+++ src/minimalistic_native_cljs/app/core.cljs
@@ -31,4 +31,4 @@
                :style  {:width 80 :height 80 :margin-bottom 30}}]
        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
                              :on-press #(handle-click "success")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Press Me~"]]])))
+        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "Press Me"]]])))
