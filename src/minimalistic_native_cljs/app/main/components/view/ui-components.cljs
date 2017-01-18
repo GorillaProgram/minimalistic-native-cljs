600ad760b0c5b6a84dbae75f9a27d7d357e779d9
MeePwn <maybewaityou@gmail.com>
2017-01-18 09:05:02 +0800

    first commit

--- src/minimalistic_native_cljs/app/main/components/view/ui-components.cljs
+++ src/minimalistic_native_cljs/app/main/components/view/ui-components.cljs
@@ -3,17 +3,25 @@
 
 (def ReactNative (js/require "react-native"))
 
+; ============================== Common components start ====================================
 (def app-registry (.-AppRegistry ReactNative))
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def image (r/adapt-react-class (.-Image ReactNative)))
+(def picker (r/adapt-react-class (.-Picker ReactNative)))
+(def picker-item (r/adapt-react-class (.-Picker.Item ReactNative)))
+(def list-view (r/adapt-react-class (.-ListView ReactNative)))
+(def scroll-view (r/adapt-react-class (.-ScrollView ReactNative)))
+(def input (r/adapt-react-class (.-TextInput ReactNative)))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+(def navigator (r/adapt-react-class (.-Navigator ReactNative)))
+; ============================== Common components end ====================================
 
 (defn show-alert
   [message]
   (.alert (.-Alert ReactNative) message))
 
 (defn simple-component
-  [args]
+  [params]
   [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} (args :text)]])
+   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} (params :text)]])
