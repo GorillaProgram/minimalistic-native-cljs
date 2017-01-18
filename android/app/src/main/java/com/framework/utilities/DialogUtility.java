8b57b1fc59a097c6dde68febf55ab45de49cf157
MeePwn <maybewaityou@gmail.com>
2017-01-18 13:49:57 +0800

    first commit

---
+++ android/app/src/main/java/com/framework/utilities/DialogUtility.java
@@ -0,0 +1,16 @@
+package com.framework.utilities;
+
+/**
+ * package: com.framework.utilities
+ * author: MeePwn
+ * email: maybewaityou@gmail.com
+ * github: https://github.com/maybewaityou
+ * date: 2017/1/18 下午1:49
+ * desc:
+ */
+
+public class DialogUtility {
+
+
+
+}
