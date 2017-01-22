256527da3527aa43bb784499eeeb2593271546dc
MeePwn <maybewaityou@gmail.com>
2017-01-22 09:05:42 +0800

    first commit

--- src/minimalistic_native_cljs/app/main/modules/network-module.cljs
+++ src/minimalistic_native_cljs/app/main/modules/network-module.cljs
@@ -1,5 +1,6 @@
 (ns minimalistic-native-cljs.app.main.modules.network
   (:require [reagent.core :as r :refer [atom]]
+            [cognitect.transit :as t]
             [minimalistic-native-cljs.app.main.modules.native-modules :refer [native-modules]]
             [minimalistic-native-cljs.app.main.utilities.data.json-utility :refer [to-json to-string]]))
 
@@ -13,3 +14,6 @@
                   (to-string (clj->js params))
                   #(success-callback (js->clj (to-json %)))
                   #(failure-callback (js->clj (to-json %)))))
+
+(def r (t/reader :json))
+(def w (t/writer :json))
